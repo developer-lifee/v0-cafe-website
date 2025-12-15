bf79206f4164e122cbbfc4cea4da20a4ee4baba9
developerlifee <eavila@uniempresarial.edu.co>
2025-12-14 23:53:54 -0500

    Refactor next.config.mjs to clarify output configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // ESTA LÍNEA ES LA CLAVE para generar la carpeta 'out/'
+  output: 'export', 
+  // ========================================================
   typescript: {
     ignoreBuildErrors: true,
   },
